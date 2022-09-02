4b95c7e4e7f699618f7c8dfd7be0f40d23c696c0
Ryan Etedali <ryanetedali@gmail.com>
2022-09-02 14:04:26 -0500

    modified address's models

--- backend/addresses/models.py
+++ backend/addresses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+    street = models.CharField(max_length=500)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.IntegerField()
+
+    
