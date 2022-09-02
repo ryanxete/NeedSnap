41ddbc6c11a13c326e0b9f94531bbe79541bee10
Ryan Etedali <ryanxete@Ryans-MacBook-Pro.local>
2022-09-02 12:57:07 -0500

    created serializers for new folders + modified users

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     is_provider = models.BooleanField(default=False)
     phone_number = models.IntegerField()
+    address_id = models.ForeignKey(on_delete=models.CASCADE)
+    service_id = models.ForeignKey(on_delete=models.CASCADE)
+
+
     
     # Example (note import of models above that is commented out)
     # this will add a column to the user table
