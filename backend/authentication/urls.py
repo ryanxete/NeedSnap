08ee131babcc0e9d27a7c4a4c442006624672aad
Ryan Etedali <ryanetedali@gmail.com>
2022-09-09 11:46:00 -0400

    added new function to the auth views and the related endpoint

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, MyTokenObtainPairView
+from .views import ChangeUserServiceView, RegisterView, MyTokenObtainPairView
 
 urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('service/', ChangeUserServiceView.as_view(), name='change_service')
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import get_user_model
 from .serializers import MyTokenObtainPairSerializer, RegistrationSerializer
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.http import HttpRequest
 User = get_user_model()
 
 
@@ -14,3 +17,19 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
+
+class ChangeUserServiceView(TokenRefreshView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        user = User.objects.get(id=request.user.id)
+        user.service_id = request.data['service_id']
+        user.save()
+
+        serializer = MyTokenObtainPairSerializer(user)
+        token = serializer.get_token(user)
+        req = HttpRequest()
+        req.__setattr__('data', {"refresh": str(token)})
+
+        return super().post(req)
