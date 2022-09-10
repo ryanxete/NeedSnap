bfaa5874032b40339939f29fee1a2efc7f7bddba
Ryan Etedali <ryanetedali@gmail.com>
2022-09-09 20:28:25 -0400

    created more components

---
+++ frontend/src/components/About/WWeDoC.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WWeDoC() {
+  return (
+    <div>WWeDoC</div>
+  )
+}
+
+export default WWeDoC
---
+++ frontend/src/components/About/WWeDoP.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WWeDoP() {
+  return (
+    <div>WWeDoP</div>
+  )
+}
+
+export default WWeDoP
